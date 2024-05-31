24552be57267a57af9346721ee7a11c0fe9ae2c4
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2024-05-31 19:04:04 +0300

    Add GlobalExceptionHandler

--- src/SharedKernel/SharedKernel/Http/GlobalExceptionHandler.cs
+++ src/SharedKernel/SharedKernel/Http/GlobalExceptionHandler.cs
@@ -1,30 +1,35 @@
-﻿//using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-//namespace SharedKernel.Http;
+namespace SharedKernel.Http;
 
-//public sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
-//{
-//    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
+public sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
 
-//    public async ValueTask<bool> TryHandleAsync(
-//        HttpContext httpContext,
-//        Exception exception,
-//        CancellationToken cancellationToken)
-//    {
-//        _logger.LogError(
-//            exception, "Exception occurred: {Message}", exception.Message);
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(
+            exception,
+            "Exception occurred: {Message}",
+            exception.Message);
 
-//        var problemDetails = new ProblemDetails
-//        {
-//            Status = StatusCodes.Status500InternalServerError,
-//            Title = "Server error"
-//        };
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = exception.Message
+        };
 
-//        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
 
-//        await httpContext.Response
-//            .WriteAsJsonAsync(problemDetails, cancellationToken);
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
 
-//        return true;
-//    }
-//}
+        return true;
+    }
+}
